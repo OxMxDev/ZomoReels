aa5e9b9566c25b6b62c802a45917dc7e082bb8a9
Om <omdwivedi234@gmail.com>
2025-09-03 22:19:05 +0530

    building

---
+++ backend/src/routes/auth.routes.js
@@ -0,0 +1,7 @@
+const express = require("express");
+const router = express.Router()
+const authController = require("../controllers/auth.controller")
+
+router.post('/user/register',authController.registerUser)
+router.post('/user/login',authController.loginUser)
+module.exports = router
